63f5608dfa84a2757791cf1d5687d77cfe3a575c
Igli milaqi <milaqiigli@gmail.com>
2018-07-27 13:26:02 +0200

    intial commit

---
+++ Assets/RotateScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+
+	[SerializeField] 
+	private float rotationSpeed;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate(0,0,-rotationSpeed);
+	}
+}
