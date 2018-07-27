a832feb34a61d14cee16b171f3577b102be331b4
Igli milaqi <milaqiigli@gmail.com>
2018-07-27 13:57:21 +0200

    Now the square doesnt need to have a parent to rotate around. Also started implementing jumping mechanics for now using spring joints.

--- Assets/JumpScript.cs
+++ Assets/JumpScript.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
-public class JumpScript : MonoBehaviour {
+public class JumpScript : MonoBehaviour
+{
+	
+	[SerializeField] 
+	[Range(0,10)]
+	private float jumpSpeed = 0.2f;
 
+	private Rigidbody2D rb;
+	private SpringJoint2D joint;
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		joint = GetComponent<SpringJoint2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		if (Input.GetMouseButton(0))
+		{
+			rb.AddForce(transform.up * jumpSpeed);
+			joint.distance = 2.4f;
+		}
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			joint.distance = 2.16f;
+		}
 	}
 }
