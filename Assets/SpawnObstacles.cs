7227fe49534f6efd6c34a02003f549c7cc360326
Igli milaqi <milaqiigli@gmail.com>
2018-07-27 17:24:31 +0200

    Added basic obstacle spawning.

---
+++ Assets/SpawnObstacles.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacles : MonoBehaviour
+{
+
+	[SerializeField] 
+	private GameObject obstacle;
+
+	private float amountOfObstacles = 2;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		SpawnObstacle();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	private void SpawnObstacle()
+	{
+		Vector2 _center = transform.position;
+		
+		for (int i = 0; i < 5; i++)
+		{
+			Vector2 _pos = Random.insideUnitCircle.normalized * transform.localScale.x;
+			Quaternion _rot = Quaternion.FromToRotation(Vector3.up,_center - _pos);
+			
+			Instantiate(obstacle, _pos, _rot);
+		}
+	}
+}
