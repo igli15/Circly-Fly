7359f4cfb01468a66521adfbd85cd879c9a54d1e
Igli milaqi <milaqiigli@gmail.com>
2018-08-02 11:53:49 +0200

    Added objectPooler script and some sprites to test gems/

---
+++ Assets/Scripts/SpawnGems.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGems : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject gem;
+
+	private List<GameObject> gemList;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		gemList = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
