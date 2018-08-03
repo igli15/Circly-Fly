eefe623b69ac056905d59778f9e05cb89df9b7c9
Igli milaqi <milaqiigli@gmail.com>
2018-08-03 21:32:36 +0200

    Added a new effect when a lap is completed.

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerData : MonoBehaviour {
+public class PlayerData : MonoBehaviour
+{
 
+	private int highscore;
+	
+	private int tier3GemCount;
+
+	private int tier2GemCount;
+
+	private int tier1GemCount;
+	
+	
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
+	}
+
+	public int GetHighscore()
+	{
+		return highscore;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public int GetTier3GemCount()
+	{
+		return tier3GemCount;
 	}
+	
+	public int GetTier2GemCount()
+	{
+		return tier2GemCount;
+	}
+	
+	public int GetTier1GemCount()
+	{
+		return tier1GemCount;
+	}
+
+	public void Save()
+	{
+		JsonUtility.ToJson(this);
+	}
+
+	public void Load(string savedData)
+	{
+		JsonUtility.FromJsonOverwrite(savedData,this);
+	}
+	
+	
 }
